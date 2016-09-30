185c9edaf3c9ebe2a3e4e1c8568d4e544eb14e22
Campesan <ana-raquel.campesan@hpe.com>
2016-09-30 17:33:55 -0300

    hooked up get urls

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+from restless.fl import FlaskResource
+from restless.preparers import FieldsPreparer
 
 app = Flask(__name__)
-current_dir = os.getcwd()
+# Database relative path (three slashes)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./cep_database.db'
 db = SQLAlchemy(app)
 
+# Model
 class Cep(db.Model):
     cep = db.Column(db.String(10), primary_key=True)
     # logradouro = db.Column(db.String(50))
@@ -19,3 +21,20 @@ class Cep(db.Model):
 
     def __repr__(self):
         return '<CEP %r>' % self.cep
+
+# Restless resource
+class CepResource(FlaskResource):
+    preparer = FieldsPreparer(fields={
+        'cep': 'cep'
+    })
+
+    # GET
+    def list(self):
+        return Cep.query.all()
+
+    # GET specific
+    def detail(self, pk):
+        return Cep.query.filter_by(cep=pk).first()
+
+# URLs Routes
+CepResource.add_url_rules(app, rule_prefix='/cep/')
